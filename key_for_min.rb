b9951ebcbb3e5a7ae6302c91cd6cf06198bf48c8
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-08 19:38:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  least = "" #sets least as an empty string to initialize
+  name_hash.each {|key, value| #iterates over each key/value pair
+      if (least == "" || least > value)
+        least = value
+      end
+     }
+  name_hash.key(least)
 end
+
+
+#in our first loop since "least" is equal to an empty string,
+#reassign "least" to the value from our first key/value pair.
+#all subsequent loops are then comparing "least" to the next value in thei key/value pair.
+#if "least" is greater than the new value, least is again reassigned to our new value
+#we iterate over all values, leaving "least" as equal to the lowest value
+#we then call the #key method on the value of "least" to return only the least key.
+#for the #key method, if the value is not found it will return nil.
